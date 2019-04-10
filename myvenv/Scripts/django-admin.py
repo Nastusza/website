486333fbea1140f71f0cd8667790c1ff1d5860dd
Natalia <natalia_i@tlen.pl>
2019-04-10 09:49:11 +0200

    porzadek w kodzie

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\asoloma\desktop\programowanie\djangoblog\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
